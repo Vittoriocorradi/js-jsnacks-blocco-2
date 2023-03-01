54a346b194f5703cca186ab148fe550a230fbbb4
Vittoriocorradi <vittoriocorradi2310@gmail.com>
2023-03-01 11:14:44 +0100

    Fix While Js

--- snack1/js/main.js
+++ snack1/js/main.js
@@ -11,13 +11,15 @@ for (let i = 0; i < 5; i++) {
 console.log(`Somma For finale è ${sumFor}`);
 
 let sumWhile = 0;
-let index = 0;
+let index = 1;
 
-while (index < 5) {
-    const inputWhile = Number(prompt('Inserisci un numero'));
-    sumWhile += inputWhile;
-    console.log(sumWhile);
-    index++;
+while (index <= 5) {
+    const inputWhile = Number(prompt(`Inserisci un numero (${index})`));
+    if (!isNaN(inputWhile)) {
+        sumWhile += inputWhile;
+        console.log(sumWhile);
+        index++;
+    }
 }
 
 console.log(`Somma While finale è ${sumWhile}`);
